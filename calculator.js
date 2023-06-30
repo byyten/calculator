8ee7ecd4c09c8ceb1aff6c775ae8fa78f8cab7aa
Craig <craiggordon@hotmail.com>
2023-06-30 18:18:46 +1200

    Add and run basic testing and refine math evaluation

--- calculator.js
+++ calculator.js
@@ -1,25 +1,50 @@
-    const run_evaluation = (expression) => {
-        let re = /\s\+\s|\s\-\s/g
-        let groups = expression.split(re)
-        let group_ops = expression.match(re)
+    // const run_evaluation = (expr) => {
+    //   console.log(typeof expr)
+    //     let re = /\s\+\s|\s\-\s/g
+    //     let groups = expr.split(/\s\+\s|\s\-\s/g)
+    //     let group_ops = expr.match(re)
         
-        console.log([groups.length, group_ops.length])
+    //     console.log([groups.length, group_ops.length])
         
-        let total = group_evaluate(groups[0].split(' '))
-        console.log(total)
+    //     let total = group_evaluate(groups[0].split(' '))
+    //     console.log(total)
         
-        groups.slice(1).forEach((grp, idx) => {
-          // if (idx) {
-            val = group_evaluate(grp.split(' '))
-            console.log(val)
-            if (group_ops[idx] == '+') {
-              total += val
-            } else {
-              total -= val
-            }
-          // }
-        })
-        return total
+    //     groups.slice(1).forEach((grp, idx) => {
+    //       // if (idx) {
+    //         val = group_evaluate(grp.split(' '))
+    //         console.log(val)
+    //         if (group_ops[idx] == '+') {
+    //           total += val
+    //         } else {
+    //           total -= val
+    //         }
+    //       // }
+    //     })
+    //     return total
+    // }
+
+
+    const run_evaluation = (expr) => {
+
+      re = /\s\+\s|\s\-\s/g
+      groups = expr.split(re)
+      group_ops = expr.match(re) 
+      
+      console.log([groups.length, group_ops.length])
+
+      total = group_evaluate(groups[0].split(' '))
+      console.log(total)
+
+      groups.slice(1).forEach((grp, idx) => {
+          val = group_evaluate(grp.split(' '))
+          console.log([group_ops[idx], val])
+          if (group_ops[idx] === ' + ') {
+          total += val
+          } else if (group_ops[idx] === ' - ' ) {
+          total -= val
+          }
+      })
+      return total;
     }
 
     const group_evaluate = (grp) => {
@@ -35,7 +60,7 @@
                 grp_total = grp_total % _g[1]
             } 
         }
-        return grp_total
+        return Number(grp_total)
     }
 
 
--- calculator.spec.js
+++ calculator.spec.js
@@ -1,9 +1,25 @@
 const calculator = require('./calculator');
 
+
+describe('eval simple 1', () => {
+    let expr = '2 * 8 + 3'
+    console.log(expr)
+	test('eval ' + expr.toString(), () => {
+        console.log(typeof expr)
+		expect(calculator.run_evaluation(expr)).toBe(eval(expr));
+	});
+
+});
+
+
+
+
 describe('eval', () => {
-    exp = '-82 * 658 - -254 + 23 - 68 + 379'
-	test('eval ' + exp.toString(), (exp) => {
-		expect(calculator.run_evaluation(exp)).toBe(eval(exp));
+    let expr = '-82 * 658 - 254 + -23 - 68 + 379'
+    console.log(expr)
+	test('eval ' + expr.toString(), () => {
+        console.log(typeof expr)
+		expect(calculator.run_evaluation(expr)).toBe(eval(expr));
 	});
 
 });
